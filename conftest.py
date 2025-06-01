e005a8e5e5971823e56979d19674f6863af70d07
redwanatef <redwan.atef.02@gmail.com>
2025-06-01 16:31:59 +0300

    fix: make scope of page session instead of page

--- conftest.py
+++ conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import sync_playwright,  Browser
+from playwright.sync_api import sync_playwright, Browser, Page
 
 @pytest.fixture(scope="session")
 def browser():
@@ -9,12 +9,16 @@ def browser():
         yield browser
         browser.close()
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def page(browser: Browser):
-    """Create a new page for each test."""
+    """Create a single page for all tests in the session."""
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://app.turath.io/")
     yield page
     page.close()
     context.close()
+
+@pytest.fixture(autouse=True)
+def navigate_homepage(page: Page):
+    """Automatically navigate to the homepage before each test."""
+    page.goto("https://app.turath.io/")
